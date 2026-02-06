299c13fd3c19f5696995f4604a8c27d61d6a4f76
Ganesh-Gani-dev <hganesh1909@gmail.com>
2026-02-06 11:20:02 +0530

    Add Header and CoreConcepts components to structure the app

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,37 +1,12 @@
-import reactImg from "./assets/react-core-concepts.png";
+import Header from "./components/Header.jsx";
 import JSXimg from "./assets/jsx-ui.png";
 import stateImg from "./assets/state-mgmt.png";
 import propsImg from "./assets/config.png";
 import componentsImg from "./assets/components.png";
+import CoreConcepts from "./components/Components.jsx"
 
-const reactDescriptions = ["Fundamental", "Core", "Crucial"];
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[getRandomInt(2)]} React concepts you will need for
-        almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
