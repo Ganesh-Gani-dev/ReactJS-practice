0c91c12a064ae7986351227a98a048cf7cda156d
Ganesh-Gani-dev <hganesh1909@gmail.com>
2026-02-06 17:07:19 +0530

    Add TabButton component and examples section to App

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,11 +4,19 @@ import stateImg from "./assets/state-mgmt.png";
 import propsImg from "./assets/config.png";
 import componentsImg from "./assets/components.png";
 import CoreConcepts from "./components/Components.jsx"
-
+import TabButton from "./components/TabButton .jsx"
+import { useState } from "react";
+import {EXAMPLES} from "./data.js"
 
 
 
 function App() {
+  const[selectedTopic,setSelectedTopic] = useState("components");
+  function handleSelect(selectedButton)
+  {
+    setSelectedTopic(selectedButton)
+    console.log(`${selectedButton} has been clicked`); 
+  }
   return (
     <div>
       <Header> </Header>
@@ -37,6 +45,27 @@ function App() {
               image={stateImg}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic === "components"} onSelect={()=>{
+              //console.log('on select function is under execution')
+              handleSelect("components")
+              }}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onSelect={()=>{handleSelect("jsx")}}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onSelect={()=>handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onSelect={()=>handleSelect("state")}>State</TabButton>
+          </menu>
+           <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <br />
+            <code>
+              {EXAMPLES[selectedTopic].code}
+            </code>
+           </div>
+          
         </section>
       </main>
     </div>
