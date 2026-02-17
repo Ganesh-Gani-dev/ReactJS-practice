eb64de68455e25ffb52e3be71276add255bd226d
Ganesh-Gani-dev <hganesh1909@gmail.com>
2026-02-17 21:17:29 +0530

    Initial setup to add a new project

--- 12-starting-project/src/App.jsx
+++ 12-starting-project/src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
---
+++ 12-starting-project/src/components/NewProject.jsx
@@ -0,0 +1,24 @@
+import Input from "./Input.jsx";
+import Button from "./Button.jsx";
+
+export default function NewProject() {
+  return (
+    <div className="w-[35rem] mt-16">
+      <div>
+        <Input label="Title" />
+        <Input label="Description" textarea />
+        <Input label="Due Date" />
+      </div>
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <Button>Save</Button>
+        </li>
+      </menu>
+    </div>
+  );
+}
