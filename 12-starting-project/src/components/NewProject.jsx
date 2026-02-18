b46867431db57f6998bf2ddbb130a622841d11fc
Ganesh-Gani-dev <hganesh1909@gmail.com>
2026-02-18 11:50:06 +0530

    Projects being added

--- 12-starting-project/src/components/NewProject.jsx
+++ 12-starting-project/src/components/NewProject.jsx
@@ -1,13 +1,29 @@
+import { useRef } from "react";
 import Input from "./Input.jsx";
 import Button from "./Button.jsx";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
   return (
     <div className="w-[35rem] mt-16">
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
@@ -16,7 +32,7 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <Button>Save</Button>
+          <Button onClick={handleSave}>Save</Button>
         </li>
       </menu>
     </div>
